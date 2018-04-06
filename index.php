7371cee7d5be26cc211ea5ce6db2f265e440a402
Michael van der Heide <vanderheide.michael@gmail.com>
2018-04-06 16:16:04 +0200

    Fixed output of page content

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
 <?php
 // This file is the main file of pingery elm
 // This file is used to show the log and execute the website checks
+// IMPORTANT DO NOT CREATE ANY FUNCTIONS!!!
+
+
+
+
+
+// Add Code here for calling websites and creating the log data
+
+
+
+
+
+//Code to create HMTL page content
+//Replaces default values in index.html
+$HTML = file_get_contents('html/index.html', FILE_USE_INCLUDE_PATH);
+$HTML = str_replace('[elm_Login_Link]', '..\\manage.php', $HTML);
+$HTML = str_replace('[elm_Login_Text]', 'Manage Websites', $HTML);
+$HTML = str_replace('[elm_Page_NavBar]', '<a class="active">Pingery elm</a>', $HTML);
+
+//Replace this with log information!!!
+$HTMLContent = '';
+
+//Gives out the html
+$HTML = str_replace('[elm_Page_Content]', $HTMLContent, $HTML);
+echo $HTML;
 
 ?>
--- manage.php
+++ manage.php
@@ -1,4 +1,22 @@
 <?php
 // This file is the secondary file of pingery elm
 // This is file is used to show the different existing websites and give the possibility to delete or add them
+
+
+//Code to create HMTL page content
+//Replaces default values in index.html
+$HTML = file_get_contents('html/index.html', FILE_USE_INCLUDE_PATH);
+$HTML = str_replace('[elm_Login_Link]', '..\\index.php', $HTML);
+$HTML = str_replace('[elm_Login_Text]', 'See log', $HTML);
+$HTML = str_replace('[elm_Page_NavBar]', '<a class="active">Pingery elm - Manage websites</a>', $HTML);
+
+//Replace this with:
+//- Add / Delete functions for Websites
+//- Overview of all Websites
+$HTMLContent = '';
+
+//Gives out the html
+$HTML = str_replace('[elm_Page_Content]', $HTMLContent, $HTML);
+echo $HTML;
+
 ?>
