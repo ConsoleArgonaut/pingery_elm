5af0f4e808b8e773bc2ef88053c6aefb2d8de524
Unknown <laura.steiner@nyp.ch>
2018-04-13 14:27:37 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- index.php
+++ index.php
@@ -16,7 +16,6 @@
 //Code to create HMTL page content
 //Replaces default values in index.html
 $HTML = file_get_contents('html/index.html', FILE_USE_INCLUDE_PATH);
-$HTML = str_replace('[elm_Login_Link]', '..\\manage.php', $HTML);
 $HTML = str_replace('[elm_Login_Text]', 'Manage Websites', $HTML);
 $HTML = str_replace('[elm_Page_NavBar]', '<a class="active">Pingery elm</a>', $HTML);
 
