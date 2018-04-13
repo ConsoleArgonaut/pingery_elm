ae1930d495b5b4dfe89db88e06f1ca3dd6b0254a
Michael van der Heide <vanderheide.michael@gmail.com>
2018-04-13 14:11:07 +0200

    Created API, ready to use. No Database connection yet

--- manage.php
+++ manage.php
@@ -15,6 +15,14 @@ $HTML = str_replace('[elm_Page_NavBar]', '<a class="active">Pingery elm - Manage
 //- Overview of all Websites
 $HTMLContent = '';
 
+//Gets Content from API
+$websites = json_decode(file_get_contents('http://' . $_SERVER['HTTP_HOST'] . '/api/websites/get.php'), true);
+
+foreach($websites as $url => $name) {
+    //@Laura. Overview of all the websites here
+    $HTMLContent = $HTMLContent . '<a href="' . $url . '">' . $name . '</a></br>';
+}
+
 //Gives out the html
 $HTML = str_replace('[elm_Page_Content]', $HTMLContent, $HTML);
 echo $HTML;
