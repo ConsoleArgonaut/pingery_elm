61988788423100f1bc2979b81a19f7a422de98a3
LEGACY\Drake <kaiden3991@gmail.com>
2018-04-17 20:04:18 +0200

    same as previous

--- api/websites/get.php
+++ api/websites/get.php
@@ -5,20 +5,17 @@
 
 // Defines that this is a Json Web Service
 header('Content-Type: application/json');
-
+$conn4 = new PDO($elm_Settings_DSN, $elm_Settings_DbUser, $elm_Settings_DbPassword, array(
+    PDO::ATTR_PERSISTENT => true
+));
 //Gets Websites from Database
 $websites = array();
-$sql = $conn->prepare("SELECT * FROM elm_websites;");
+$sql = $conn4->prepare("SELECT * FROM elm_websites;");
 $pages = $sql->execute();
 foreach ($pages as $page) {
     $pushObject = $page['URL']." => ".$page['Name'];
     array_push($websites, $pushObject);
 }
-// Creates array of websites
-//$websites = array(
-//    "http://php.net" => "Official PHP Website",
-//    "https://stackoverflow.com" => "Stackoverflow -> questions and answers"
-//);
 
 // Creation and output of Json data
 echo json_encode($websites);
