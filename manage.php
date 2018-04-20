5c8d1e114797707b3aeb737f633d62dd65062507
Lyndwyn <laura.steiner@nyp.ch>
2018-04-20 08:26:05 +0200

    cleaned dev-comments

--- manage.php
+++ manage.php
@@ -2,7 +2,6 @@
 // This file is the secondary file of pingery elm
 // This is file is used to show the different existing websites and give the possibility to delete or add them
 
-
 //region Base HTML Creation
 //Code to create HMTL page content
 //Replaces default values in index.html
@@ -11,9 +10,6 @@ $HTML = str_replace('[elm_Login_Link]', 'index.php', $HTML);
 $HTML = str_replace('[elm_Login_Text]', 'See log', $HTML);
 $HTML = str_replace('[elm_Page_NavBar]', '<a class="active">Pingery elm - Manage websites</a>', $HTML);
 
-//Replace this with:
-//- Add / Delete functions for Websites
-//- Overview of all Websites
 $HTMLContent = '';
 //endregion
 
